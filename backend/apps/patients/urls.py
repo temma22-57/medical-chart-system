4951e69db32d44f54461febf8709967b7ea7cfdf
Tristan Emma <temma22.57@gmail.com>
2026-04-14 21:56:14 -0700

    File cleanup

--- backend/apps/patients/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from .views import PatientListCreateView
-
-urlpatterns = [
-    path("", PatientListCreateView.as_view(), name="patients"),
-]
--- backend/apps/patients/views.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import generics
-from .models import Patient
-from .serializers import PatientSerializer
-
-class PatientListCreateView(generics.ListCreateAPIView):
-    queryset = Patient.objects.all().order_by("-created_at")
-    serializer_class = PatientSerializer
